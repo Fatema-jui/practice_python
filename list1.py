a3f628abdd9edd3e6a97f3fe1d34931d3fbd8750
Fatema-jui <asratulmontaha@gmail.com>
2026-09-09 15:52:34 +0600

    shopping_list_manager,list1

---
+++ list1.py
@@ -0,0 +1,16 @@
+fruits = ["banana", "apple", "orange", "grape", "kiwi"]
+item = [True, 5.2, "yo"]
+
+print(fruits[0])
+print(fruits[1][4])
+print(item[2][1])
+
+#dictionary
+person = {
+    "name": "Alice", 
+    "age": 30, 
+    "city": "New York"
+    }
+print(person)
+print(person["name"])
+print(type(person["name"]))
